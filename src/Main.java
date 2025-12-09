8b67746ec17f868ba951b61703b5137cc886c431
QiQianChen <Qiqianc@nycstudents.net>
2025-12-09 15:27:51 -0500

    .

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        String[] teams = {"Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"};
+        int[] teamsWins = new int[7];
+        String team1 = "";
+        String team2 = "";
+        Scoreboard match;
+        String winnerName = "";
+        File f = new File("Scoreboard.txt");
+        Scanner s = new Scanner(f);
+        while (s.hasNext()){
+            team1 = s.next();
+            team2 = s.next();
+            match = new Scoreboard(team1, team2);
+            while (s.hasNextInt()) match.recordPlay(s.nextInt());
+            winnerName = match.returnWinner();
+            for (int i = 0; i < teams.length; i++){
+                if (winnerName.equals(teams[i])) {
+                    teamsWins[i] += 1;
+                }
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < teams.length; i++){
+            System.out.println(teams[i] + " "+ teamsWins[i]);
+        }
+    }
 }
--- src/Scoreboard.java
+++ src/Scoreboard.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class Scoreboard {
+    private String team1;
+    private String team2;
+    private int team1Points = 0;
+    private int team2Points = 0;
+    private String activeTeam;
+    public Scoreboard(String team1, String team2) {
+        this.team1 = team1;
+        this.team2 = team2;
+        activeTeam = team1;
+    }
+    public void recordPlay(int points) {
+        if (activeTeam.equals(team1)) {
+            if (points == 0) activeTeam = team2;
+            team1Points += points;
+        } else {
+            if (points == 0) activeTeam = team1;
+            team2Points += points;
+        }
+    }
+    public String returnWinner(){
+        if (team1Points > team2Points) return team1;
+        else if (team2Points > team1Points) return team2;
+        return "";
+    }
 }
